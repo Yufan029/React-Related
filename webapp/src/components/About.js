d110c78a09ba67d94d1e2bd92881db9e87c7ca68
Yufan Zhang <zhang.yufan029@gmail.com>
2019-04-14 23:44:53 +1000

    New Web App

---
+++ webapp/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem quisquam voluptate illo sint cumque, quasi, labore aspernatur, neque nam minus nobis similique est quae omnis saepe. Officiis ipsum provident rem?</p>
+        </div>
+    )
+}
+
+export default About;
