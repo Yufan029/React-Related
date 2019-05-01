14418f59cb425e9227bae0f8f69a23d9a90b2f55
Allen Zhang <Allen.Zhang@transmax.com.au>
2019-05-01 13:47:52 +1000

    Create other pages and add materialize js file to enable label for input tag.

---
+++ react-redux-firebase-list-project/src/components/auth/SignIn.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class SignIn extends Component {
+    state = {
+        email: '',
+        password: '',
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div className="container sign-in section">
+                <h5 className="grey-text text-darken-2 center">Sign In</h5>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="input-field">
+                        <i className="material-icons prefix grey-text text-lighten-1">email</i>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name="email" id="email" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <i className="material-icons prefix grey-text text-lighten-1">keyboard</i>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name="password" id="password" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field right">
+                        <button className="btn indigo lighten-1 z-depth-0">Login</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
---
+++ react-redux-firebase-list-project/src/components/projects/CreateProject.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+export default class CreateProject extends Component {
+    state = {
+        title: '',
+        content: '',
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    render() {
+        return (
+            <div className="container section create-project">
+                <h5 className="grey-text lighten-1 center">Create a New Project</h5>
+                <form onSubmit={this.handleSubmit}>
+                    <div className="input-field">
+                        <i className="material-icons prefix grey-text lighten-1">title</i>
+                        <label htmlFor="title">Project Title</label>
+                        <input type="text" name="title" id="title" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <i className="material-icons prefix grey-text lighten-1">drafts</i>
+                        <label htmlFor="content">Project Content</label>
+                        <textarea className="materialize-textarea" name="content" id="content" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <button className="btn indigo lighten-1 right">Create</button>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
