0363644dccd55e372709ec9da4dbeee0b17e228a
Yufan029 <zhang.yufan029@gmail.com>
2023-10-30 14:14:58 +1000

    Delete react-redux-firebase-list-project directory

--- react-redux-firebase-list-project/src/store/actions/projectActions.js
+++
@@ -1,6 +0,0 @@
-export const createProject = (project) => {
-    return (dispatch, getState) => {
-        // add async call to fetch the data
-        dispatch({ type: 'CREATE_PROJECT', project });
-    }
-}
